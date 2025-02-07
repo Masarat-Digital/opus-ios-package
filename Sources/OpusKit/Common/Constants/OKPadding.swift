9280243abbd5e8263cfca739c924a9e1418b76e1
Jakub Legut <jakub-legut@wp.pl>
2025-02-07 20:44:48 +0100

    OpusKit Add OKPadding sizes

--- Sources/OpusKit/Common/Constants/OKPadding.swift
+++ Sources/OpusKit/Common/Constants/OKPadding.swift
@@ -8,3 +8,11 @@ public enum OKPadding {
     static let huge = CGFloat(24)
 }
 
+// TODO: JL Check common sizes and keep them
+
+public extension OKPadding {
+    static let size16 = CGFloat(16)
+    static let size32 = CGFloat(32)
+    static let size64 = CGFloat(64)
+}
+
